f299f63d8166ab3b2a798478373dde3d57d07bcb
loiq.amonbekov <amonbekov.loiq@gmail.com>
2023-03-19 13:43:19 +0500

    Initial commit

---
+++ resources/js/Components/Welcome.vue
@@ -0,0 +1,16 @@
+<script setup>
+import ApplicationLogo from '@/Components/ApplicationLogo.vue';
+</script>
+
+<template>
+    <div>
+        <div class="p-6 lg:p-8 bg-white border-b border-gray-200">
+
+
+
+
+        </div>
+
+
+    </div>
+</template>
---
+++ resources/js/Pages/AdminApp/Field/Index.vue
@@ -0,0 +1,11 @@
+<template>
+ascknakscnaslcnka
+</template>
+
+<script setup>
+
+</script>
+
+<style scoped>
+
+</style>
